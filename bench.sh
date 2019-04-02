7177e4badc9365d18a3c7f29fcf9974a9fc31cb6
beabevi <beatricebevilacqua1995@gmail.com>
2019-04-02 20:10:29 +0200

    Implemented histogram and data aggregation

--- bench.sh
+++ bench.sh
@@ -9,8 +9,8 @@ wait_if_skipped() {
 
 groups=$(./bin/likwid-perfctr -a | tail -n +3 | cut -b 1-15)
 groups=($groups)
-group_flags=$(printf -- "-g %s " "${groups[@]}")
-flags="-t 100ms"
+group_flags="-g UOPS_RETIRE -g L3 -g DATA -g FLOPS_AVX -g L2CACHE -g CYCLE_ACTIVITY -g TLB_INSTR -g MEM -g MEM_DP -g FLOPS_DP -g UOPS -g TLB_DATA -g CLOCK -g L2 -g MEM_SP -g ENERGY -g ICACHE -g BRANCH -g RECOVERY -g DIVIDE -g UOPS_EXEC -g L3CACHE -g FLOPS_SP -g FALSE_SHARE -g TMA -g UOPS_ISSUE -g CYCLE_STALLS"
+flags="-T 100ms"
 command="./bin/likwid-perfctr $group_flags $flags"
 
 # Disable Turbo Boost
@@ -29,42 +29,42 @@ for f in 1.0 1.5 2.2; do
     fi
 
     echo "$(date): FPU core bound $t - $f"
-    $command $cpus ./bin/likwid-bench -t ddot_sp -w S0:16kB:$t -i 15000000 > "data/FPU-core-$t-$f.stdout" 2> "data/FPU-core-$t-$f.stderr"
+    $command -o data_aggregated/FPU-core-$t-$f.csv $cpus ./bin/likwid-bench -t ddot_sp -w S0:16kB:$t -i 15000000 > "data_aggregated/FPU-core-$t-$f.stdout"
     echo $(ps -e | grep likwid)
     wait_if_skipped
 
     echo "$(date): FPU mem bound $t - $f"
-    $command $cpus ./bin/likwid-bench -t ddot_sp -w S0:1GB:$t -i 200 > "data/FPU-mem-$t-$f.stdout" 2> "data/FPU-mem-$t-$f.stderr"
+    $command -o data_aggregated/FPU-mem-$t-$f.csv $cpus ./bin/likwid-bench -t ddot_sp -w S0:1GB:$t -i 200 > "data_aggregated/FPU-mem-$t-$f.stdout"
     echo $(ps -e | grep likwid)
     wait_if_skipped
 
     echo "$(date): Copy cache $t - $f"
-    $command $cpus ./bin/likwid-bench -t copy -w S0:16kB:$t -i 30000000 > "data/Copy-cache-$t-$f.stdout" 2> "data/Copy-cache-$t-$f.stderr"
+    $command -o data_aggregated/Copy-cache-$t-$f.csv $cpus ./bin/likwid-bench -t copy -w S0:16kB:$t -i 30000000 > "data_aggregated/Copy-cache-$t-$f.stdout"
     echo $(ps -e | grep likwid)
     wait_if_skipped
 
     echo "$(date): Copy mem $t - $f"
-    $command $cpus ./bin/likwid-bench -t copy -w S0:1GB:$t -i 250 > "data/Copy-mem-$t-$f.stdout" 2> "data/Copy-mem-$t-$f.stderr"
+    $command -o data_aggregated/Copy-mem-$t-$f.csv $cpus ./bin/likwid-bench -t copy -w S0:1GB:$t -i 250 > "data_aggregated/Copy-mem-$t-$f.stdout"
     echo $(ps -e | grep likwid)
     wait_if_skipped
 
     echo "$(date): ALU core bound $t - $f"
-    $command $cpus ./bin/likwid-bench -t sum_int -w S0:16kB:$t -i 10000000 > "data/ALU-core-$t-$f.stdout" 2> "data/ALU-core-$t-$f.stderr"
+    $command -o data_aggregated/ALU-core-$t-$f.csv $cpus ./bin/likwid-bench -t sum_int -w S0:16kB:$t -i 10000000 > "data_aggregated/ALU-core-$t-$f.stdout"
     echo $(ps -e | grep likwid)
     wait_if_skipped
 
     echo "$(date): ALU mem bound $t - $f"
-    $command $cpus ./bin/likwid-bench -t sum_int -w S0:1GB:$t -i 150 > "data/ALU-mem-$t-$f.stdout" 2> "data/ALU-mem-$t-$f.stderr"
+    $command -o data_aggregated/ALU-mem-$t-$f.csv $cpus ./bin/likwid-bench -t sum_int -w S0:1GB:$t -i 150 > "data_aggregated/ALU-mem-$t-$f.stdout"
     echo $(ps -e | grep likwid)
     wait_if_skipped
 
     echo "$(date): AVX $t - $f"
-    $command $cpus ./bin/likwid-bench -t ddot_sp_avx -w S0:16kB:$t -i 75000000 > "data/AVX-cache-$t-$f.stdout" 2> "data/AVX-cache-$t-$f.stderr"
+    $command -o data_aggregated/AVX-cache-$t-$f.csv $cpus ./bin/likwid-bench -t ddot_sp_avx -w S0:16kB:$t -i 75000000 > "data_aggregated/AVX-cache-$t-$f.stdout"
     echo $(ps -e | grep likwid)
     wait_if_skipped
 
     echo "$(date): Scattered $t - $f"
-    $command $cpus ./bin/likwid-bench -t copy_scattered -w S0:1GB:$t -i 50000  > "data/Scattered-$t-$f.stdout" 2> "data/Scattered-$t-$f.stderr"
+    $command -o data_aggregated/Scattered-$t-$f.csv $cpus ./bin/likwid-bench -t copy_scattered -w S0:1GB:$t -i 50000 > "data_aggregated/Scattered-$t-$f.stdout"
     echo $(ps -e | grep likwid)
     wait_if_skipped
 
---
+++ histogram.py
@@ -0,0 +1,93 @@
+import argparse
+
+import matplotlib.pyplot as plt
+
+from pathlib import Path
+
+
+def get_group_name_and_id(line):
+    l = line.split(",")
+    return l[2], int(l[1].split()[1])
+
+def get_groups(file):
+    with open(file) as f:
+        lines = f.readlines()
+        table_lines = get_line_indices_starting_with(lines, "TABLE")
+        return sorted(set([get_group_name_and_id(lines[i]) for i in table_lines]), key=lambda t: t[1])
+
+def get_line_indices_starting_with(lines, string):
+    indices = []
+    for i, l in enumerate(lines):
+        l = l.strip()
+        if l.startswith(string):
+            indices.append(i)
+    return indices
+
+def get_group_lines(file, group):
+    with open(file) as f:
+        lines = f.readlines()
+        groups_indices = get_line_indices_starting_with(lines, "STRUCT")
+        
+        if group == len(groups_indices) - 1:
+            return lines[groups_indices[group] + 1:]
+        else:
+            return lines[groups_indices[group] + 1 : groups_indices[group + 1]]
+
+def get_table_lines(group_lines):
+    table_indices = get_line_indices_starting_with(group_lines, "TABLE")
+    index = 2 if len(table_indices) == 4 else 1
+    if index == len(table_indices) - 1:
+        return group_lines[table_indices[index] + 1 : ]
+    else:
+        return group_lines[table_indices[index] + 1 : table_indices[index + 1]]
+
+aggregations = {
+    "sum": sum,
+    "min": min,
+    "max": max,
+    "avg": lambda a: sum(a) / len(a),
+    "any": lambda a: a[0]
+}
+
+def get_field(table_lines, field, type):
+    fields = table_lines[0].strip(",\n").split(",")
+    core_idxs = get_line_indices_starting_with(fields, "Core")
+    line = table_lines[get_line_indices_starting_with(table_lines, field)[0]]
+    line = line.strip(",\n").split(",")
+    vals = [float(line[i]) for i in core_idxs]
+    print(vals)
+    return aggregations[type](vals)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Plot data")
+    parser.add_argument("file", type=str, nargs='+', help="Path to the file without extension")
+    parser.add_argument("-g", "--group", type=int, default=1)
+    parser.add_argument("-m", "--metric", type=str)
+    parser.add_argument("-t", "--type", type=str, default=None)
+    parser.add_argument("-p", "--print-groups", action="store_true")
+    parser.add_argument("-c", "--core", default=0)
+    args = parser.parse_args()
+
+    if args.print_groups:
+        groups = get_groups(args.file[0])
+        for g in groups:
+            print(str(g[1]) + ":" + g[0])
+        exit(0)
+
+    if args.group and not args.type:
+        print("".join(get_group_lines(args.file[0], args.group - 1)))
+        exit(0)
+
+    fields = []
+    for file in args.file:
+        table = get_table_lines(get_group_lines(file, args.group - 1))
+        field = get_field(table, args.metric, args.type)
+        fields.append(field)
+
+
+    x = list(range(len(args.file)))
+    plt.bar(x, height=fields, color=[plt.get_cmap("tab20")(i) for i in range(len(args.file))])
+    plt.xticks(x,[Path(file).with_suffix("").name for file in args.file], rotation=45)
+    plt.ylabel(args.metric)
+    plt.show()
